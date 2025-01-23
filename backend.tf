db782b7246c766777b80c53c4772765292a315c3
Nasser Gonzalez Gonzalez <nasser.gonzalez@harness.io>
2025-01-23 09:54:16 +0000

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "kunduso-terraform-remote-bucket"
-    encrypt = true
-    key     = "tf/add-aws-elb-ec2-terraform/terraform.tfstate"
-    region  = "us-east-2"
-  }
-}
